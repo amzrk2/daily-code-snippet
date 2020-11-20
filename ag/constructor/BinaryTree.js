ed2ac3ba4eb93c6cf2feb81e027603231cb5c1a9
amzrk2 <amzrk2@outlook.com>
2020-11-20 16:29:44 +0800

    update: jz_04

--- ag/constructor/BinaryTree.js
+++ ag/constructor/BinaryTree.js
@@ -19,13 +19,29 @@ class BinaryTree {
    * @param {array} vinArr mid order arr
    */
   constructor(preArr, vinArr) {
-    this.root = new BinaryTreeNode(preArr[0], null, null);
-    const rootIndex = vinArr.indexOf(preArr[0]);
-    console.log(vinArr.splice(0, rootIndex - 1), vinArr.splice(1));
-    // this.buildTree(vinArr.splice(0, rootIndex - 1), vinArr.splice(1));
+    this.root = this.buildTree(preArr, vinArr);
   }
 
-  buildTree(leftArr, rightArr) {}
+  /**
+   * @param {array} preArr pre order arr
+   * @param {array} vinArr mid order arr
+   */
+  buildTree(preArr, vinArr) {
+    if (preArr.length <= 0 || vinArr.length <= 0) {
+      return null;
+    }
+    // get current root
+    const rootVal = preArr[0];
+    // get root index
+    const rootIndex = vinArr.indexOf(rootVal);
+    // build tree with left child's pre and mid order arr
+    const leftChild = this.buildTree(preArr.slice(1, rootIndex + 1), vinArr.slice(0, rootIndex));
+    // build tree with right child's pre and mid order arr
+    const rightChild = this.buildTree(preArr.slice(rootIndex + 1), vinArr.slice(rootIndex + 1));
+    // build node
+    const node = new BinaryTreeNode(rootVal, leftChild, rightChild);
+    return node;
+  }
 }
 
 module.exports = {
